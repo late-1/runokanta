a5763f5094d4d5a96cf10305399a3659949ba401
late-1 <lauri.tuominen11@outlook.com>
2025-11-04 01:57:06 +0200

    add functions for registration and login

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask
+from flask import abort, flash, make_response, redirect, render_template, request, session
+
+import users
+import secrets
+import sqlite3
+
+app = Flask(__name__)
+app.secret_key = "Kissa123"
+
+@app.route("/")
+def index():
+    if "user_id" in session:
+        return render_template("index.html")
+    return redirect("/login")
+
+   # all_poems = poems.get_all()
+   # return render_template("index.html"), poems=all_poems)
+
+def require_login():
+    if "user_id" not in session:
+        abort(403)
+
+
+def check_csrf():
+    if "csrf_token" not in request.form:
+        abort(403)
+    if request.form["csrf_token"] != session["csrf_token"]:
+        abort(403)
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        flash("VIRHE: salasanat eivät ole samat")
+        return redirect("/register")
+
+    try:
+        users.create_user(username, password1)
+    except sqlite3.IntegrityError:
+        flash("VIRHE: tunnus on jo varattu")
+        return redirect("/register")
+
+    return redirect("/")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user_id = users.check_login(username, password)
+        if user_id:
+            session["user_id"] = user_id
+            session["username"] = username
+            session["csrf_token"] = secrets.token_hex(16)
+            return redirect("/")
+        else:
+            flash("VIRHE: väärä tunnus tai salasana")
+            return redirect("/login")
+        
+@app.route("/logout")
+def logout():
+    if "user_id" in session:
+        del session["user_id"]
+        del session["username"]
+    return redirect("/")
