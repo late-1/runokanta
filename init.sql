9879e61ddd4150a2f0eef9099a934da1bd1a1d28
late-1 <lauri.tuominen11@outlook.com>
2025-11-04 00:56:04 +0200

    init and db.py for initializing and connecting to database

---
+++ init.sql
@@ -0,0 +1,16 @@
+DELETE FROM category;
+DELETE FROM themes;
+
+INSERT INTO category (title, value) VALUES ('laji', 'runo');
+INSERT INTO category (title, value) VALUES ('laji', 'haiku');
+INSERT INTO category (title, value) VALUES ('laji', 'sonetti');
+INSERT INTO category (title, value) VALUES ('laji', 'proosaruno');
+INSERT INTO category (title, value) VALUES ('laji', 'tanka');
+
+INSERT INTO themes (title, value) VALUES ('teema', 'rakkaus');
+INSERT INTO themes (title, value) VALUES ('teema', 'luonto');
+INSERT INTO themes (title, value) VALUES ('teema', 'yksinäisyys');
+INSERT INTO themes (title, value) VALUES ('teema', 'ilo');
+INSERT INTO themes (title, value) VALUES ('teema', 'suru');
+INSERT INTO themes (title, value) VALUES ('teema', 'ystävyys');
+INSERT INTO themes (title, value) VALUES ('teema', 'aika');
